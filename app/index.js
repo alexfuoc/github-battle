7d50fdcee0ceafdd4e34836d19a1205136868c29
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2020-04-22 21:06:37 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
       return (
          <div>
             <div>
-               <Battle />
+               <Popular />
             </div>
          </div>
       )
